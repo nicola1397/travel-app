cea9ac13619d3fb8a874aff2a219d470b9591143
nicola1397 <nicola1397@outlook.it>
2024-08-31 20:37:24 +0200

    Deploy test 1

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from './components/Header.vue'
 export default {
   data() {
     return {
-      images: ['url(./public/castle.jpg)', 'url(./public/jungle.jpg)', 'url(./public/sea.jpg)'],
+      images: ['url("public/castle.jpg")', 'url("public/jungle.jpg")', 'url("public/sea.jpg")'],
       currentIndex: 0
     }
   },
@@ -40,7 +40,7 @@ export default {
   background-size: cover;
   height: 100vh;
   width: 100vw;
-  --background-image: url(./public/castle.jpg);
+  --background-image: url('/public/castle.jpg');
 }
 #appBackground::before {
   content: '';
